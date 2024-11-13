9d7c47128ec6b1333488f36f3c40a3f0473ea424
LeeSeoHyeong1031 <champ36912@naver.com>
2024-11-13 22:28:12 +0900

    no message

--- Assets/20241113NavMesh/Scripts/PlayerMovement.cs
+++ Assets/20241113NavMesh/Scripts/PlayerMovement.cs
@@ -1,56 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.AI.Navigation;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent nav;
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float rotSpeed;
-    private Vector3 moveDir = Vector3.zero;
-    private float eulerAngleX;
-    private float eulerAngleY;
+	[SerializeField] private NavMeshAgent nav;
+	public Transform point;
 
-    private void Start()
-    {
-        nav.speed = moveSpeed;
-    }
-
-    private void Update()
-    {
-        moveDir.x = Input.GetAxis("Horizontal");
-        moveDir.z = Input.GetAxis("Vertical");
-        moveDir = moveDir.normalized;
-
-        eulerAngleX += Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime;
-        eulerAngleY += Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime;
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit, 1000f))
-            {
-                transform.position = hit.point;
-            }
-        }
-
-        Move(moveDir);
-        Rotate();
-    }
-
-    private void Move(Vector3 dir)
-    {
-        transform.Translate(dir * nav.speed * Time.deltaTime);
-    }
-
-    private void Rotate()
-    {
-        eulerAngleY = Mathf.Clamp(eulerAngleY, -30f, 30f);
-        transform.localRotation = Quaternion.Euler(-eulerAngleY, eulerAngleX, 0);
-    }
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out RaycastHit hit, 1000f, 1 << LayerMask.NameToLayer("Plane")))
+			{
+				nav.SetDestination(hit.point);
+			}
+		}
+	}
 }
