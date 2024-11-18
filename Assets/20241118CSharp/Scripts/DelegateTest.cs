e2f0f00c90953b6133e562c60c8dedc6326b34c2
LeeSeoHyeong1031 <champ36912@naver.com>
2024-11-18 17:02:41 +0900

    no message

---
+++ Assets/20241118CSharp/Scripts/DelegateTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DelegateTest : MonoBehaviour
+{
+    [SerializeField] private InputField inputField;
+    [SerializeField] private InputField inputField2;
+    [SerializeField] private Button operatorButton;
+    [SerializeField] private Button calcButton;
+    [SerializeField] private Text resultText;
+
+    private int inputNum;
+    private int inputNum2;
+
+
+}
